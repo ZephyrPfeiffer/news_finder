880c4b960f17e4a42ae006253215655a00879272
Zephyr Pfeiffer <zephyrpfeiffer@gmail.com>
2024-11-13 00:51:24 -0500

    feature/allow user request specific game news information

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require("express");
 // express
 const app = express();
 
+app.use(express.json());
+
 // listen for requests
 app.listen(3000);
 
@@ -16,11 +18,18 @@ app.get("/gamenews", async (req, res) => {
   );
   const steamGames = await response.json();
 
-  const gameToFind = "omega strikers".toLowerCase();
+  let newsRequestParameters = {
+    gameName: "",
+    newsCount: 3,
+    newsLength: 1000,
+    ...req.body,
+  };
+
+  console.log(req.body);
 
   // intialize variable to hold first edit distance
   let minEditDistance = getEditDistance(
-    gameToFind,
+    newsRequestParameters.gameName.toLowerCase(),
     steamGames.applist.apps[0].name.toLowerCase()
   );
 
@@ -31,35 +40,48 @@ app.get("/gamenews", async (req, res) => {
   };
 
   // iterate through game info to find game name with lowest edit distance to user provided game name
-  for (let i = 1; i < data.applist.apps.length; i++) {
+  for (let i = 1; i < steamGames.applist.apps.length; i++) {
     let editDistance = getEditDistance(
-      gameToFind,
+      newsRequestParameters.gameName.toLowerCase(),
       steamGames.applist.apps[i].name.toLowerCase()
     );
 
-    // if game name is exact match, return the current game
+    // if game name is exact match, update gameInfo object and exit loop
     if (editDistance === 0) {
       gameInfo.appid = steamGames.applist.apps[i].appid;
       gameInfo.gameName = steamGames.applist.apps[i].name;
       break;
     }
-    if (editDistance < currentMinValue) {
-      currentMinValue = editDistance;
+    if (editDistance < minEditDistance) {
+      minEditDistance = editDistance;
       gameInfo.appid = steamGames.applist.apps[i].appid;
       gameInfo.gameName = steamGames.applist.apps[i].name;
     }
   }
 
-  console.log(gameInfo);
-
   // fetch news for specified game
   const newsData = await fetch(
-    `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${gameInfo.appid}&count=10&maxlength=1000&format=json`
+    `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${gameInfo.appid}&count=${newsRequestParameters.newsCount}&maxlength=${newsRequestParameters.newsLength}&format=json`
   );
 
   const gameNews = await newsData.json();
 
-  res.json(gameNews);
+  // create initial response object
+  let requestedInformation = {
+    gameName: gameInfo.gameName,
+    newsItems: [],
+  };
+
+  // populate response object with relevent game news information
+  for (let i = 0; i < gameNews.appnews.newsitems.length; i++) {
+    requestedInformation.newsItems.push({
+      title: gameNews.appnews.newsitems[i].title,
+      author: gameNews.appnews.newsitems[i].author,
+      url: gameNews.appnews.newsitems[i].url,
+      newsContent: gameNews.appnews.newsitems[i].contents,
+    });
+  }
+  res.json(requestedInformation);
 });
 
 app.use((req, res) => {
